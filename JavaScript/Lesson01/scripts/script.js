a2f7beea078fe4a7e9a9e3f2ab87ad4cbf83ed3c
Yurii <yuriimyronets@gmail.com>
2018-06-17 22:43:21 +0300

    js lesson 01

---
+++ JavaScript/Lesson01/scripts/script.js
@@ -0,0 +1,59 @@
+/*
+var userName = prompt("Введите Ваше имя");
+var year = parseInt(prompt("Введите год рождения "));
+var age = 2018 - year;
+alert("Здравствуйте " + userName + " Ваш возраст " + age + " лет/год.");
+*/
+
+/*
+var widthRoom = parseFloat(prompt("Введите ширину комнаты (м.)"));
+var lenRoom = parseFloat(prompt("Введите длину комнаты (м.)"));
+var heightRoom = parseFloat(prompt("Введите высоту комнаты (м.)"));
+
+var winH = parseFloat(prompt("Введите высоту окна (м.)"));
+var winW = parseFloat(prompt("Введите ширину окна (м.)"));
+
+var doorH = parseFloat(prompt("Введите высоту двери (м.)"));
+var doorW = parseFloat(prompt("Введите ширину двери (м.)"));
+
+var rollW = parseFloat(prompt("Введите ширину рулона (м.)"));
+var rollLen = parseFloat(prompt("ведите длину рулона (м.)"));
+
+var squareRoomFull = (widthRoom * heightRoom + lenRoom * heightRoom) * 2;
+var squareWindow = winH * winW;
+var squareDoor = doorH * doorW;
+var squareRoom = squareRoomFull - (squareWindow + squareDoor);
+var squareRoll = rollW * rollLen;
+var numberRoll = squareRoom / squareRoll;
+numberRoll.toFixed(2);
+alert("Необходимо " + numberRoll + " рулонов");
+*/
+
+/*
+var userNumber = parseInt(prompt("Enter the number: "));
+alert(+(userNumber>10));
+*/
+
+/*
+var userNumber = parseFloat(prompt("Enter the number: "));
+var balance = userNumber - parseInt(userNumber);
+alert( );
+*/
+
+/*
+var a = 10;
+var b = a>10?"text":"other text";
+alert(b);
+*/
+
+
+/*
+var volumeBath = parseInt(prompt("Enter volume of bath"));
+var volumeBucket = parseInt(prompt("Enter volume of bucket"));
+var bucketCount = parseInt(prompt("Enter count of buckets"));
+
+var volumeTotal = volumeBucket * bucketCount;
+
+var result = volumeTotal>volumeBath?"will not fit":"fit in";
+alert(result);
+*/

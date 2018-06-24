29a4333b569d0a8b0ee8eae2454fe76a3bdbaf6d
Yurii <yuriimyronets@gmail.com>
2018-06-24 13:08:15 +0300

    js hw02

---
+++ JavaScript/Homeworks/02/scripts/script.js
@@ -0,0 +1,51 @@
+//task 0
+
+var answerCount = 0;
+
+var userAnswer = parseInt(prompt("Enter the number where '1' is '5', '2' is '8', '3' is '-2'. 2+3=?"));
+if (userAnswer == 1) {
+    answerCount++
+}
+var userAnswer = parseInt(prompt("Enter the number where '1' is '20', '2' is '5', '3' is '11'. 8+3=?"));
+if (userAnswer == 3) {
+    answerCount++
+}
+var userAnswer = parseInt(prompt("Enter the number where '1' is '12', '2' is '-13', '3' is '9'. 20-8=?"));
+if (userAnswer == 1) {
+    answerCount++
+}
+var userAnswer = parseInt(prompt("Enter the number where '1' is '51', '2' is '62', '3' is '45'. 40+5=?"));
+if (userAnswer == 3) {
+    answerCount++
+}
+var userAnswer = parseInt(prompt("Enter the number where '1' is '76', '2' is '88', '3' is '-25'. 79+9=?"));
+if (userAnswer == 2) {
+    answerCount++
+}
+var userAnswer = parseInt(prompt("Enter the number where '1' is '50', '2' is '62', '3' is '33'. 79-17=?"));
+if (userAnswer == 2) {
+    answerCount++
+}
+var userAnswer = parseInt(prompt("Enter the number where '1' is '23', '2' is '54', '3' is '71'. 80-9=?"));
+if (userAnswer == 3) {
+    answerCount++
+}
+var userAnswer = parseInt(prompt("Enter the number where '1' is '64', '2' is '24', '3' is '38'. 27+11=?"));
+if (userAnswer == 3) {
+    answerCount++
+}
+var userAnswer = parseInt(prompt("Enter the number where '1' is '9', '2' is '15', '3' is '12'. 3*3=?"));
+if (userAnswer == 1) {
+    answerCount++
+}
+var userAnswer = parseInt(prompt("Enter the number where '1' is '79', '2' is '44', '3' is '16'. 88/2=?"));
+if (userAnswer == 2) {
+    answerCount++
+}
+answerCount*= 1.2;
+alert("Your mark is " + parseInt(answerCount));
+
+//task 1
+
+
+

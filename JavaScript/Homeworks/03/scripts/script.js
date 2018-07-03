7960020d1d0ea230dab67a54520b4eeffb77144e
Yurii <yuriimyronets@gmail.com>
2018-07-04 01:02:56 +0300

    js hw 03

--- JavaScript/Homeworks/03/scripts/script.js
+++ JavaScript/Homeworks/03/scripts/script.js
@@ -1,7 +1,22 @@
-i=1;
+//Task 0
+
+var i=1;
 while (i<=100) {
     if (i%3!==0){
         document.write(i+"<br>");
     }
     i++;
 }
+
+//Task 1
+
+var x = parseInt(prompt("Enter the number"));
+var i = 1;
+var j = 0;
+while (i<=100) {
+    if (i%x===0)  {
+        j+=j++;?
+    }
+    i++;
+}
+document.write("Sum of numbers: "+j);

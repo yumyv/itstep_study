b24d8bdcbcd62456f2ed234220452f476ae0e84f
Yurii <yuriimyronets@gmail.com>
2018-08-05 18:12:16 +0300

    js hw 04

--- JavaScript/Homeworks/04/scripts/script.js
+++ JavaScript/Homeworks/04/scripts/script.js
@@ -1,5 +1,38 @@
 //task 01
 
+var height = parseInt(prompt("Enter the height"));
+var n = "&nbsp"
+for (let i = 0; i < height; i++) {
+    for (let k = height; k > i - 1; k--) {
+        document.write(n, n)
+    }
+    for (let j = 0; j < i + 1; j++) {
+        document.write("*", n, n)
+    }
+    document.write("<br>")
+}
+
+
+var w = 2 * parseInt(prompt("Enter the height"));
+var h = (w - (w % 2)) / 2 + (w % 2);
+var spaces = w;
+
+for (i = h; i > 0; i--) {
+    var str = "";
+    for (j = i; j <= w; j++) {
+        if (i === 1) {
+            str += "*";
+        } else if (j === w || j === spaces) {
+            str += "*";
+        } else {
+            str += "&nbsp&nbsp";
+        }
+    }
+    spaces -= 2;
+    document.write(str + "<br/>");
+}
+
+
 //task 02
 
 let answer = parseInt(Math.random() * 100);
@@ -11,7 +44,7 @@ while (attempt <= attemptsCount) {
     if (userAnswer === "q" || userAnswer === "Q") break;
     else if (userAnswer > answer) alert("Your answer is too large. You have " + (attemptsCount - attempt) + " attempts");
     else if (userAnswer < answer) alert("Your answer is too small. You have " + (attemptsCount - attempt) + " attempts");
-    else if (userAnswer === answer) {
+    else if (userAnswer == answer) {
         let result = confirm("You won! Do you wanna play again?");
         if (result) break;
     }

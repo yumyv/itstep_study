f384d01b265f4ab3b29bcb0ccf1e084b1f8d0075
Yurii <yuriimyronets@gmail.com>
2018-11-27 16:05:05 +0200

    js hw 25

--- JavaScript/Homeworks/25/scripts/main.js
+++ JavaScript/Homeworks/25/scripts/main.js
@@ -1,3 +1,23 @@
 jQuery(document).ready(function () {
-    
+    class Notes {
+        constructor() {
+            this.notes = [];
+        }
+
+
+
+        addNote (note) {
+            this.notes.push(note);
+        }
+    }
+
+    class Note {
+        constructor (name, description) {
+            this.name = name;
+            this.description = description;
+        }
+
+
+    }
+
 });

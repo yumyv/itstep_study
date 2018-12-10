165f1e578f7873da5242b74e1a58d4fda8d37510
Yurii <yuriimyronets@gmail.com>
2018-12-10 10:39:41 +0200

    java hw03

---
+++ Java/Homeworks/03/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import com.company.entities.User;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        User user = new User();
+
+        while (true) {
+            System.out.println("1 - add user");
+            System.out.println("2 - delete user by number");
+            System.out.println("3 - delete user by name");
+            System.out.println("4 - show list");
+
+            int choice = Integer.parseInt(sc.nextLine());
+
+            switch (choice) {
+                case 1:
+                    user.addUser(sc.nextLine());
+                    break;
+                case 2:
+                    user.deleteUserByIndex();
+                    break;
+                case 3:
+                    user.deleteUserByName();
+                    break;
+                case 4:
+                    user.showList();
+                    break;
+            }
+        }
+    }
+}
